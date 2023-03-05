23ab9595a9a05b9e44fedd22dfa5e7a6e81e59e4
White11010 <beliavski26@gmail.com>
2023-03-05 14:39:10 +0300

    remove complete TODOs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,3 @@
-// TODO
-// 2. Связать вместе размеры поля в модели и вью
-// 5. Добавить классы для Змеи и Еды
-// 6. Занести интерфейсы и енумы в соответсвующие директории
-
-
 import type { IAppPresenter } from './presenter';
 import { AppPresenter } from './presenter';
 import { AppModel } from './model';
